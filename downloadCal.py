c4e5fb06ef7fcb693fc3e8cd2dc895b7e22f5823
Bobby Mushkot <bobby.mushkot@gmail.com>
2019-07-20 19:08:51 -0700

    Updated and corrected html and css, added a button for testing future javascript features

--- downloadCal.py
+++ downloadCal.py
@@ -4,6 +4,6 @@ import time
 starttime= time.time()
 while True:
     print("tick")
-    data = urllib.request.urlretrieve('$LINK_FOR_CALENDAR', 'testcalfile.ics')
-    # Every x seconds we sleep 
+    data = urllib.request.urlretrieve('http://p49-caldav.icloud.com/published/2/MTA0NTA4NTI4MDcxMDQ1MMs_O0LBF_P0lr5FF_4-eR9Mv-NuzkyEVCg3_zJuyGtT', 'testcalfile.ics')
+    # Every x seconds we sleep
     time.sleep(60.0 - ((time.time() - starttime) % 60.0))
