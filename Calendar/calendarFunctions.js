a07f13ee8ddbfd378bf479214d231d0cf562a0a2
Bobby Mushkot <bobby.mushkot@gmail.com>
2019-07-22 17:25:42 -0700

    Updated sizing for html and css and added skeleton for main js function

--- Calendar/calendarFunctions.js
+++ Calendar/calendarFunctions.js
@@ -2,3 +2,27 @@ function clicked(){
     document.getElementById("monthID").innerHTML = "December";
     document.getElementById("yearID").innerHTML = "2019";
 }
+
+
+
+// Main Skeleton
+/*
+Wait Refresh time
+Check Current Month & Year
+    Update Calendar Appropriately
+
+Open ICS file
+    if(line == "begin VEVENT")
+        while(line != "end VEVENT")
+            analyzeLine(line, object(VEVENT)) // This will take in the line and
+                                              // put the appropriate info into
+                                              // the object.
+            next line
+        put object into vevent array
+
+    go through vevent array and insert items into current months days
+
+*/
+function main(){
+
+}
