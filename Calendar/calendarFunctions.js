7d4e552e874abba56b9f0f956f436702cfc679ad
Bobby Mushkot <bobby.mushkot@gmail.com>
2019-07-26 18:59:09 -0700

    finished calendarFunctions.js to change the days displayed correctly for each month

--- Calendar/calendarFunctions.js
+++ Calendar/calendarFunctions.js
@@ -1,6 +1,5 @@
 function setMonth(day){
     var month = day.getMonth();
-    // Maybe add a monthDays [31, 28, 30...etc]
     const monthNames = ["January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December"];
 
@@ -13,18 +12,65 @@ function setYear(day){
 }
 
 /*
-*   This function will read the current month and determine 
+*   This function will read the current month and determine
 *   how the days should be alligned.
-*   i.e In what column/row cell should the first and last of the month be 
+*   i.e In what column/row cell should the first and last of the month be
 *   as well as all the other days in between.
 *   This should also set the empty cells before and after the first and last
 *   of the month to the appropriate days of the previous and following month.
-*   Example: The first of the month falls on a tuesday, the monday and sunday 
-*   cells should be the last 2 days of the previous month. 
+*   Example: The first of the month falls on a tuesday, the monday and sunday
+*   cells should be the last 2 days of the previous month.
 */
-function updateCalendarDays(){
+function updateCalendarDays(day){
+    const monthTotalDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var currentMonth = day.getMonth();
+    var currentYear = day.getFullYear();
 
+    var firstOfMonth = new Date(currentYear, currentMonth, 1, 1, 1, 1, 1);
+    var dayFirstOfMonth = firstOfMonth.getDay(); // Gets the day of the week
+
+    // This will be the first x squares of the calendar (ie last months days)
+    var lastOfPrevMonth =  new Date(currentYear, currentMonth - 1,
+                                monthTotalDays[currentMonth - 1], 1, 1, 1, 1);
+    var dayLastOfPrevMonth =  lastOfPrevMonth.getDay(); // Gets the day of the
+                                                       // week of the last day
+                                                       // of the previous month
+    var dayNum = 1;
+    var prevMonthDayNum = monthTotalDays[currentMonth-1] - dayLastOfPrevMonth;
+    var nextMonthDayNum = 1;
+
+    // if(dayLastOfPrevMonth == 6){
+    //     dayLastOfPrevMonth = -1);
+    // }
+
+    for(var row = 0; row < 5; row++){
+        for(var col = 0; col < 7; col++){
+
+            // If we are before the start of the month then we take the last
+            // months days and set to disabled
+            if(col <= dayLastOfPrevMonth && row == 0 && dayFirstOfMonth !== 0){
+                document.getElementById("" + row + " " + col).innerHTML = prevMonthDayNum;
+                document.getElementById("" + row + " " + col).className = "day day--disabled"
+                prevMonthDayNum++;
+
+            }
+            // The days of the current month
+            else if(dayNum <= monthTotalDays[currentMonth]){
+                document.getElementById("" + row + " " + col).innerHTML = dayNum;
+                dayNum++;
+            }
+            // If we are after the start of the month then we take the next
+            // months days and set to disabled
+            else{
+                document.getElementById("" + row + " " + col).innerHTML = nextMonthDayNum;
+                document.getElementById("" + row + " " + col).className = "day day--disabled"
+                nextMonthDayNum++;
+            }
+
+        }
+    }
 }
+
 
 
 // Main Skeleton
@@ -47,17 +93,15 @@ Open ICS file
 */
 function main(){
 
-    var today = new Date();
+    var today = new Date(2019, 8);
     setMonth(today);
     setYear(today);
-    // updateCalendarDays();
+    updateCalendarDays(today);
 
     var lineReader = require('line-reader');
-    var count = 0;
 
     lineReader.eachLine('testcalfile.ics', function(line, last) {
       console.log(line);
-      count++;
 
       if (last) {
           //document.getElementById("yearID").innerHTML = line;
